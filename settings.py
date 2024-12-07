c744c5173c4429e341e9adcfdedcb6f8821a428c
eudypte <130498542+eudypte@users.noreply.github.com>
2024-12-07 17:11:22 -0500

    style: rename font vars

--- settings.py
+++ settings.py
@@ -13,8 +13,8 @@ RED = (240, 35, 12)
 DARK_GREY = (80, 78, 81)
 BLACK = (0, 0, 0)
 planetNames = ["Sun", "Earth", "Mars", "Mercury", "Venus"]
-FONT = pygame.font.SysFont("sfmonomedium", 16)
-
+FONT_18px = pygame.font.SysFont("sfmonomedium,sfmonomediumnerdfontcomplete", 18)
+FONT_13px = pygame.font.SysFont("sfmonomedium,sfmonomediumnerdfontcomplete", 13)
 WIN = pygame.display.set_mode(( 300,500))
 
 class Settings:
@@ -25,8 +25,11 @@ class Settings:
 
     def draw(self, win):
         pygame.draw.rect(win, DARK_GREY, (20, 90, 300, 500))
-        win.blit(FONT.render('Sun', 1, WHITE),(55, 100))
-        win.blit(FONT.render('Earth', 1, WHITE),(55, 130))
-        win.blit(FONT.render('Mars', 1, WHITE),(55, 160))
-        win.blit(FONT.render('Mercury', 1, WHITE),(55, 190))
-        win.blit(FONT.render('Venus', 1, WHITE),(55, 220))
+        win.blit(FONT_18px.render('Toggle Planets', 1, WHITE),(30, 100))
+        win.blit(FONT_13px.render('Sun', 1, WHITE),(55, 132))
+        win.blit(FONT_13px.render('Earth', 1, WHITE),(55, 162))
+        win.blit(FONT_13px.render('Mars', 1, WHITE),(55, 192))
+        win.blit(FONT_13px.render('Mercury', 1, WHITE),(55, 222))
+        win.blit(FONT_13px.render('Venus', 1, WHITE),(55, 252))
+        win.blit(FONT_18px.render('Custom Planet Settings', 1, WHITE),(30, 280))
+        win.blit(FONT_13px.render('Planet Mass:', 1, WHITE),(30, 317))
