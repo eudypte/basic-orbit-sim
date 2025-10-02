7df709ca36bb04a2bbb2e01169ae75dac3e0870e
Yavuz Selim <yvzslmm4@gmail.com>
2025-10-03 00:37:20 +0300

    added comments and todo's

--- main.py
+++ main.py
@@ -13,16 +13,20 @@ pygame.init()
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 manager = pygame_gui.UIManager((WIDTH, HEIGHT))
 pygame.display.set_caption("Planet Simulation")
+# Sliders
+# TODO: fix the blue bar behind sliders
 scaleSlider = Slider(WIN, WIDTH-420, 10, 400, 30, min = 100, max = 400,color=WHITE) #380width
 newPlanetMassSlider = Slider(WIN, 35, 340, 180, 10, min = 1, max = 10,step = 0.1,color=WHITE) # * 10**24
 newPlanetYVel = Slider(WIN, 35, 390, 180, 10, min = -50, max = 50,step = 0.5, initial = 25,color=WHITE) # * 100
 
 images = Images()
 
+# Buttons Top Left
 pauseButton = button.Button(80, 20, images.pauseImg, images.resumeImg, 1)
 speedButton = button.Button(140, 20, images.spedupInactive, images.spedupActive, 1)
 settingsButton = button.Button(20, 20, images.settingsImg, images.settingsActImg, 1)
 
+# Menu Buttons
 showSun = button.Button(30, 130, images.checklistImg, images.checklistImgAct, 1)
 showEarth = button.Button(30, 160, images.checklistImg, images.checklistImgAct, 1)
 showMars = button.Button(30, 190, images.checklistImg, images.checklistImgAct, 1)
@@ -41,15 +45,16 @@ def main():
     clearPlanets = False
     clock = pygame.time.Clock()
 
-    sun = Planet(0, 0, 30, YELLOW, 1.98892 * 10 ** 30, True)
+    # TODO: rewrite in a separate function
+    sun = Planet(x=0, y=0, radius=30, color=YELLOW, mass=1.98892 * 10 ** 30, active=True)
     sun.sun = True
-    earth = Planet(-1 * Planet.AU,0,16,BLUE,5.9742 * 10**24, True)
+    earth = Planet(x=-1 * Planet.AU, y=0, radius=16, color=BLUE, mass=5.9742 * 10**24, active=True)
     earth.y_vel = 29.893 * 1000
-    mars = Planet(-1.524 * Planet.AU, 0, 12, RED, 6.39 * 10**23, True)
+    mars = Planet(x=-1.524 * Planet.AU, y=0, radius=12, color=RED, mass=6.39 * 10**23, active=True)
     mars.y_vel = 24.077 * 1000
-    mercury = Planet(0.387 * Planet.AU, 0, 8, DARK_GREY, 3.30 * 10**23, True)
+    mercury = Planet(x=0.387 * Planet.AU, y=0, radius=8, color=DARK_GREY, mass=3.30 * 10**23, active=True)
     mercury.y_vel = -47.4 * 1000
-    venus = Planet(0.723 * Planet.AU, 0, 14, WHITE, 4.8685 * 10**24, True)
+    venus = Planet(x=0.723 * Planet.AU, y=0, radius=14, color=WHITE, mass=4.8685 * 10**24, active=True)
     venus.y_vel = -35.02 * 1000
 
     planets = [sun, earth, mars, mercury, venus]
@@ -101,8 +106,9 @@ def main():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_F1:
                     debug = not debug
+            # TODO: add a menu button for custom planets and make planets more customizable
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and mouseFree == True: 
-                newP = Planet((pos[0]-(WIDTH/2))/scaleSlider.getValue() * Planet.AU, (pos[1]-(HEIGHT/2))/scaleSlider.getValue() * Planet.AU, 8, WHITE, newPlanetMassSlider.getValue() * 10**24, True)
+                newP = Planet(x=(pos[0]-(WIDTH/2))/scaleSlider.getValue() * Planet.AU, y=(pos[1]-(HEIGHT/2))/scaleSlider.getValue() * Planet.AU, radius=8, color=WHITE, mass=newPlanetMassSlider.getValue() * 10**24, active=True)
                 newP.y_vel = newPlanetYVel.getValue() * 1000
                 planets.append(newP)
 
@@ -130,6 +136,7 @@ def main():
             WIN.blit(FONT_13px.render(f"{round(newPlanetYVel.getValue().__float__(),1)} * 1000", 1, WHITE),(190, 367))
             WIN.blit(FONT_13px.render("Clear Custom Planets", 1, WHITE),(40, 430))
 
+            # TODO: rewrite this section with a helper function
             if showSun.draw(WIN, planetsAct[0]) == True:
                 planetsAct[0] = True
             else:
